4da2cf520f1033ba1d03ef3ae2d836f9823b4274
guste <gusterdehui@gmail.com>
2024-10-09 20:53:56 +0200

    fix contact.php prob

--- Contact.php
+++ Contact.php
@@ -2,20 +2,21 @@
 $metaDescription = "Page de contact de mon site";
 require_once __DIR__ . DIRECTORY_SEPARATOR . "structure.php";
 ?>
-    <h1>Contact</h1>
-    <fieldset>
-        <form action="index.php" method="post">
-            <label for="user_name">Votre prénom</label>
-            <input class="right-stink" type="text" name="user_name" id="user_name"></input><br>
-            <label for="user_surname">Votre nom</label>
-            <input class="right-stink" type="text" name="user_surname" id="user_surname"></input><br>
-            <label for="user_mail">Votre email</label>
-            <input class="right-stink" type="email" name="user_mail" id="user_mail"></input><br>
-            <label for="message">Un message sur vos impressions</label><br>
-            <input class="right-stink" type="textarea" name="message" id="message"></input><br>
-            <input type="submit" value="envoyer">
-            <input class="right-stink" type="reset" value="annuler">
-        </form>
-    </fieldset>
-    <?=$explicate?>
-    <?=$footer?>
+<?=$header?>
+<h1>Contact</h1>
+<fieldset>
+    <form action="index.php" method="post">
+        <label for="user_name">Votre prénom</label>
+        <input class="right-stink" type="text" name="user_name" id="user_name"></input><br>
+        <label for="user_surname">Votre nom</label>
+        <input class="right-stink" type="text" name="user_surname" id="user_surname"></input><br>
+        <label for="user_mail">Votre email</label>
+        <input class="right-stink" type="email" name="user_mail" id="user_mail"></input><br>
+        <label for="message">Un message sur vos impressions</label><br>
+        <input class="right-stink" type="textarea" name="message" id="message"></input><br>
+        <input type="submit" value="envoyer">
+        <input class="right-stink" type="reset" value="annuler">
+    </form>
+</fieldset>
+<?=$explicate?>
+<?=$footer?>
