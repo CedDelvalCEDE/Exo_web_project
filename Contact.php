10faba39f5452d91f1b29d19e8994d8207926de6
guste <gusterdehui@gmail.com>
2024-10-16 20:13:27 +0200

    try form work later with it

--- Contact.php
+++ Contact.php
@@ -1,22 +1,59 @@
 <?php
 $metaDescription = "Page de contact de mon site";
 require_once __DIR__ . DIRECTORY_SEPARATOR . "structure.php";
+echo $header;
+$form_data_content = ["","","",""]
 ?>
-<?=$header?>
 <h1>Contact</h1>
 <fieldset>
-    <form action="form_worker.php" method="post">
+    <form method="post">
         <label for="user_name">Votre prénom</label>
-        <input class="right-stink" type="text" name="user_name" id="user_name"></input><br>
+        <input class="right-stink" type="text" name="user_name" id="user_name" text=<?=$form_data_content[0]?> required></input><br>
         <label for="user_surname">Votre nom</label>
-        <input class="right-stink" type="text" name="user_surname" id="user_surname"></input><br>
+        <input class="right-stink" type="text" name="user_surname" id="user_surname" text=<?=$form_data_content[1]?>></input><br>
         <label for="user_mail">Votre email</label>
-        <input class="right-stink" type="email" name="user_mail" id="user_mail"></input><br>
+        <input class="right-stink" type="email" name="user_mail" id="user_mail" text=<?=$form_data_content[2]?> required></input><br>
         <label for="message">Un message sur vos impressions</label><br>
-        <input class="right-stink" type="textarea" name="message" id="message"></input><br>
+        <input class="right-stink" type="textarea" name="message" id="message" text=<?=$form_data_content[3]?> required></input><br>
         <input type="submit" value="envoyer">
         <input class="right-stink" type="reset" value="annuler">
     </form>
 </fieldset>
-<?=$explicate?>
-<?=$footer?>
+<?php
+echo $explicate;
+echo $footer;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $form_data_name = ["user_name", "user_surname", "user_mail", "message"];
+    $not_required = false;
+    $form_data = [];
+    $flag_good_entry = true;
+    foreach ($form_data_name as $index) {
+        $not_required = ($index == "user_surname") ? true : false;
+        [$min, $max] = ($index != "message") ? [2,255] : [10,3000] ;
+        if (isset($_POST[$index]) && (!empty($_POST[$index]) || $not_required)) {
+            if (good_lenght($_POST[$index], $min, $max)) {
+                $form_data[] = $_POST[$index];
+            }
+            else {
+                $form_data[] = "";
+                $flag_good_entry = false;
+            }
+        }
+        else {
+            $flag_good_entry = false;
+        }
+    }
+    if (!$flag_good_entry) {
+        $form_data_content = $form_data;
+    }
+}
+function good_lenght($entry, $min, $max) {
+    $entry_len = strlen($entry);
+    if ($entry_len > $min && $entry_len < $max) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+?>
--- form_worker.php
+++ form_worker.php
@@ -1,21 +1,39 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST["user_name"]) && !empty($_POST["user_name"])) {
-        $name = $_POST["user_name"];
+    $form_data_name = ["user_name", "user_surname", "user_mail", "message"];
+    $not_required = false;
+    $form_data = [];
+    $flag_good_entry = true;
+    foreach ($form_data_name as $index) {
+        $not_required = ($index == "user_surname") ? true : false;
+        [$min, $max] = ($index != "message") ? [2,255] : [10,3000] ;
+        if (isset($_POST[$index]) && (!empty($_POST[$index]) || $not_required)) {
+            if (good_lenght($_POST[$index], $min, $max)) {
+                $form_data[] = $_POST[$index];
+            }
+            else {
+                $flag_good_entry = false;
+            }
+        }
+        else {
+            $flag_good_entry = false;
+        }
     }
-    if (isset($_POST["user_surname"]) && !empty($_POST["user_surname"])) {
-        $surname = $_POST["user_surname"];
-    }
-    if (isset($_POST["user_mail"]) && !empty($_POST["user_mail"])) {
-        $mail = $_POST["user_mail"];
-    }
-    if (isset($_POST["message"])) {
-        $message = $_POST["message"];
+    if (!$flag_good_entry) {
+        
     }
 }
-
+function good_lenght($entry, $min, $max) {
+    $entry_len = strlen($entry);
+    if ($entry_len > $min && $entry_len < $max) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 ?>
-user_name
-user_surname
-user_mail
-message
+user_name require 2/255
+user_surname include 2/255
+user_mail require 
+message require 10/3000
